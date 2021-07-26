eee4831893b247c0dde7e3b7981291b244625752
小野紀輝 <b1915053@planet.kanazawa-it.ac.jp>
2021-07-26 11:54:46 +0900

    debug

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from linebot.exceptions import (
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
+print("---------"+ACCESS_TOKEN+"------------")
+print("---------"++CHANNEL_SECRET"------------")
+
 app = Flask(__name__)
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
