712c8064d97f58913a68ffda00df896dff254a30
小野紀輝 <b1915053@planet.kanazawa-it.ac.jp>
2021-07-26 10:53:56 +0900

    unittest

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('Hello')
+#-*- cording: utf-8 -*-
+import config
+import psycopg2
+from flask import Flask, render_template, g, request, abort
+import os
+import json
+from linebot.models import *
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+
+app = Flask(__name__)
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+    return "HelloWorld!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+@handler.add(MessageEvent, message=TextMessage)
